c87536dcfafdd1aa66ce06ade517a0bc148868e7
ecometto <ecometto@hotmail.comm>
2023-04-03 13:53:38 -0300

    sd

--- components/header.js
+++ components/header.js
@@ -20,7 +20,7 @@ function Header() {
 
       <nav className="navbar navbar-expand-md navbar-light ">
         <div className="container-fluid">
-          <a className="navbar-brand text-light" href="#">Dulce Catalina</a>
+          <a className="navbar-brand text-light" href="nosotros">Dulce Catalina</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
